9772e95212797c2ecdf430745f59bd19962e671b
htcworld <61133997+htcworld@users.noreply.github.com>
2020-03-01 07:12:46 +0400

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+module.exports = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8000,
+    allow_origin: '*'
+  }
+};
