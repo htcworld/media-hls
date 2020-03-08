3e2f9847ad9ae8bce8550481e8f46506e915837d
htcworld <61133997+htcworld@users.noreply.github.com>
2020-03-08 22:36:23 +0400

    xi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var myLogger = function (req, res, next) {
-  require('./src/server')
+  require('src/server')
   next();
 }
 
@@ -11,5 +11,7 @@ app.use(myLogger);
 app.get('/', function (req, res) {
   res.send('Hello World!');
 })
-var port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(process.env.PORT, () => {
+ console.log('app runing port 3000')
+});
+
